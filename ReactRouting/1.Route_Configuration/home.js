04f272429fe91604ab064aeb33f68243390ad828
netjs <infojtutor@gmail.com>
2025-06-02 12:27:31 +0530

    route components

---
+++ ReactRouting/1.Route_Configuration/home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+    <>
+        <h2>This is home page</h2>
+        {/* <Link to="/about">Go to About page</Link> */}
+    </>
+
+    )
+
+}
+
+export default Home;
